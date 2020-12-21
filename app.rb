3d1902a85ed07503a81aca2c0f9c7d22edd91340
Kyle Wilson <kyle@hinterlandstills.com>
2020-12-21 12:56:30 -0600

    commit tree d2b2c7e7eefac8ed8307b3743d1cebd9f8dfd964

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+    "My name is Kyle"
+end 
+
+get '/hometown' do
+    "My hometown is Crystal Lake"
+end
+
+get '/favorite-song' do
+    "My favorite song is Brighter Than Sunshine"
+end
+
+
+
+
+
 end
